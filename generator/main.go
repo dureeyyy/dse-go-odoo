a513999fc2749a57a7bb018c3b0647fea255b215
dureeyyy <andrei.palma2000@gmail.com>
2024-06-25 07:19:45 +0800

    genereated barcode.nomenclature, auth_totp_device, auth_totp_wizard, barcode_rule models

--- generator/main.go
+++ generator/main.go
@@ -15,24 +15,24 @@ func main() {
 // 	odoo "github.com/dureeyyy/go-odoo"
 // )
 
-// func main() {
-// 	c, err := odoo.NewClient(&odoo.ClientConfig{
-// 		Admin:    "admin@talinolabs.com",
-// 		Password: "J7rGSzVUoYfrloK",
-// 		Database: "seafoodcityodoo-seafoodcityodoo-production-13146340",
-// 		URL:      "https://seafoodcityodoo-seafoodcityodoo.odoo.com/",
-// 	})
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
+// // func main() {
+// // 	c, err := odoo.NewClient(&odoo.ClientConfig{
+// // 		Admin:    "admin@talinolabs.com",
+// // 		Password: "J7rGSzVUoYfrloK",
+// // 		Database: "seafoodcityodoo-seafoodcityodoo-production-13146340",
+// // 		URL:      "https://seafoodcityodoo-seafoodcityodoo.odoo.com/",
+// // 	})
+// // 	if err != nil {
+// // 		log.Fatal(err)
+// // 	}
 
-// 	criteria := odoo.NewCriteria()
+// // 	criteria := odoo.NewCriteria()
 
-// 	model, err := c.FindAccountMove(criteria)
-// 	if err != nil {
-// 		log.Fatalf("Error finding account: %v", err)
-// 	}
+// // 	model, err := c.FindAccountTax(criteria)
+// // 	if err != nil {
+// // 		log.Fatalf("Error finding account: %v", err)
+// // 	}
 
-// 	fmt.Println(model.Many2One())
+// // 	fmt.Println(model.CreateUid.ID)
 
-// }
+// // }
